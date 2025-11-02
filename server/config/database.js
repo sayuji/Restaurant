3ee5d40da25228fdba73ce82bef9baa576f2fee5
Satrio <satriobayuadji16@gmail.com>
2025-11-02 19:45:11 +0700

    y

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'restaurant_db',
+  database: 'restomaster_db',
   password: '050606',
   port: 5432,
 });
